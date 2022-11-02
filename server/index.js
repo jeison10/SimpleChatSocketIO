4c30e9506c340033aea27b93a426c7cf246bcc78
jeison10 <jeison9@gmail.com>
2022-11-01 21:30:12 -0300

    MPV com lista de usuarios

--- server/index.js
+++ server/index.js
@@ -3,9 +3,15 @@ const app = express();
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
+
+
 app.use(cors());
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./User");
 
 const server = http.createServer(app);
+
+app.use(express.json())
+
 
 const io = new Server(server, {
   cors: {
@@ -15,22 +21,55 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
-    console.log(`User Connected: ${socket.id}`);
- 
-  socket.on("join_chat", (data) => {
-    socket.join(data);
+  socket.on('join_room', ({ name, room },callback) => {
+	  
+
+      const { error, user } = addUser(
+          { id: socket.id, name, room });
+
+      if (error) return callback(error);
+
+      socket.join(user.room);
+      
+      console.log(user)
+
+            
+      io.to(user.room).emit('roomData', {
+         users: getUsersInRoom(user.room)
+     });
+
+})
    
     
-  });
-
   socket.on("send_message", (data) => {
     socket.to(data.room).emit("receive_message", data);
+	
   });
 
-  socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id);
-  });
+
+  socket.on('disconnect', () => {
+    
+    const user = removeUser(socket.id);
+    if (user) {
+      console.log(user.room)
+
+      io.to(user.room).emit('receive_message',
+      { author: 'admin', message:
+      `${user.name} had left` });
+
+      io.to(user.room).emit('roomData', {
+        users: getUsersInRoom(user.room)
+		
+		
+      
+  })}
+    
+})
+
 });
+
+
+
 
 server.listen(3001, () => {
   console.log("SERVER RUNNING");
